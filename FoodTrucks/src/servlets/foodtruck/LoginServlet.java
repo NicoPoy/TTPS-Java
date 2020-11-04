a14bf6aa7dc0a2460cdaa1b699c6af09fa79d0b2
Nicolás Poy <45112236+NicoPoy@users.noreply.github.com>
2020-11-04 20:01:41 -0300

    login funcionando

--- FoodTrucks/src/servlets/foodtruck/LoginServlet.java
+++ FoodTrucks/src/servlets/foodtruck/LoginServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import clasesDAO.DAOFactory;
+import clasesDAO.UsuarioDAO;
+import modelos.Usuario;
+
 /**
  * Servlet implementation class LoginServlet
  */
@@ -14,27 +18,34 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public LoginServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		//request.getSession(false).invalidate();
+		if( !request.getParameter("username").isEmpty() && !request.getParameter("password").isEmpty() ) {
+			UsuarioDAO uDAO = DAOFactory.getUsuarioDAO("HibernateJPA");
+			Usuario usuario = uDAO.buscarPorUsername(request.getParameter("username") );
+			if (usuario != null) {
+				if (request.getParameter("password").equals(usuario.getPassword() ) ) {
+					request.getSession(true).setAttribute("usuario", usuario);
+					System.out.println("===========================");
+					System.out.println(" <<<< Sesion iniciada >>>> ");
+					System.out.println("===========================");
+				} else { System.out.println("==========================="); 
+						System.out.println(" <<<< Contraseña incorrecta >>>> ");
+						System.out.println("==========================="); 
+						response.sendRedirect("errorPage.html"); }
+			} else { System.out.println("===========================");
+					 System.out.println(" <<<< Usuario incorrecto >>>>");
+					 System.out.println("===========================");	
+					 response.sendRedirect("errorPage.html"); }
+		}
 		doGet(request, response);
 	}
 
