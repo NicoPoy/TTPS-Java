c40938a84af4c6a9851605f988335458492c26b1
Nicolás Poy <45112236+NicoPoy@users.noreply.github.com>
2020-11-04 19:41:33 -0300

    arregaldnoErrorServlet

--- FoodTrucks/src/servlets/foodtruck/LoginServlet.java
+++ FoodTrucks/src/servlets/foodtruck/LoginServlet.java
@@ -1,4 +1,4 @@
-package servlets;
+package servlets.foodtruck;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -7,19 +7,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/ServletParaEntrega4")
-public class ServletParaEntrega4 extends HttpServlet {
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	public ServletParaEntrega4() {
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
         super();
+        // TODO Auto-generated constructor stub
     }
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
