066c02d0e658b544dba337c745f3a48c936d0759
Nicolás Poy <45112236+NicoPoy@users.noreply.github.com>
2020-11-19 22:22:43 -0300

    imports raros

--- ttps-spring-master/src/ttps/spring/clasesDAOImplHibernateJPA/GenericDAOHibernateJPA.java
+++ ttps-spring-master/src/ttps/spring/clasesDAOImplHibernateJPA/GenericDAOHibernateJPA.java
@@ -5,9 +5,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import ttps.spring.clasesDAO.GenericDAO;
 
 @Transactional
