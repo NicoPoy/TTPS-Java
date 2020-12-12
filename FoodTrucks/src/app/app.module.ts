681b13f009060bfad7a60b9ab55a7cd194007ece
Nicolás Poy <45112236+NicoPoy@users.noreply.github.com>
2020-12-12 19:35:40 -0300

    primeros componentes sin nada

--- FoodTrucks/src/app/app.module.ts
+++ FoodTrucks/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegistroComponent } from './registro/registro/registro.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
