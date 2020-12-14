c5e6797a0eac015def1b2b1b91b7e073866391fb
Nicolás Poy <45112236+NicoPoy@users.noreply.github.com>
2020-12-14 15:54:21 -0300

    Revert "back integrado"
    This reverts commit 77c500aabc6dc8c54860e0bf4de4b0d847d707a5.

--- Back/ttps-spring-master/src/ttps/spring/clasesDAO/ContratacionDAO.java
+++
@@ -1,9 +0,0 @@
-package ttps.spring.clasesDAO;
-
-import ttps.spring.model.*;
-public interface ContratacionDAO extends GenericDAO<Contratacion> {
-
-	 /*public FoodTruck devolverFoodTruck();
-	 public Evento devolverEvento();*/
-	 
-}
