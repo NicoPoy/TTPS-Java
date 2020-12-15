6e406f6aed4d34c2c85ef1f60051f3346b9c5a3e
Nicolás Poy <45112236+NicoPoy@users.noreply.github.com>
2020-12-15 15:54:32 -0300

    registra y logea

--- FoodTrucks/src/app/registro/registro.component.ts
+++ FoodTrucks/src/app/registro/registro.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Usuario } from 'src/app/modelos/usuario/usuario'
 import { ApiService } from 'src/app/servicios/api.service'
+import { AuthenticationService } from 'src/app/servicios/authentication.service'
+import { Router } from '@angular/router';
+import { ResponseI } from 'src/app/modelos/response/response-i'
 
 @Component({
   selector: 'app-registro',
@@ -10,6 +13,8 @@ import { ApiService } from 'src/app/servicios/api.service'
 })
 
 export class RegistroComponent implements OnInit {
+
+  delay = ms => new Promise(res => setTimeout(res, ms));
 
   nuevoForm = new FormGroup({
     nombre: new FormControl('', [Validators.required, Validators.minLength(3)] ),
@@ -20,14 +25,32 @@ export class RegistroComponent implements OnInit {
     zona: new FormControl ('1')
   })
 
-  constructor( private api:ApiService ) { }
+  constructor( private api:ApiService, private auth: AuthenticationService, private router:Router ) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
-  postForm( form:Usuario ){
+  async postForm( form:Usuario ){
     this.api.crearUsuario(form).subscribe(data => console.log(data) );
-    
+    console.log( "Se registro el usuario correctamente");
+
+    await this.sleep(200);
+
+    this.auth.login(form).subscribe(data => {
+      let dataResponse: ResponseI = data;
+      if (dataResponse.status == "ok") {
+        localStorage.setItem("token", dataResponse.token);
+        localStorage.setItem("userType", dataResponse.type);
+        this.router.navigate(['/home']);
+      }
+    });
+
   }
 
+  sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+
+  
 }
