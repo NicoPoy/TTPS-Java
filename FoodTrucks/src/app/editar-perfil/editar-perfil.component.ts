c1eef9d492edeb6f7c075bb53eb0a951502cde04
Tomás Cuenca <40648224+tomicuenca@users.noreply.github.com>
2020-12-16 03:13:50 -0300

    Editar perfil pero inutil :^(

--- FoodTrucks/src/app/editar-perfil/editar-perfil.component.ts
+++ FoodTrucks/src/app/editar-perfil/editar-perfil.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../modelos/usuario/usuario';
+import { ApiService } from 'src/app/servicios/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-editar-perfil',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarPerfilComponent implements OnInit {
 
-  constructor() { }
+  u = Usuario;
+
+  constructor(private api:ApiService, private router:Router) { }
 
   ngOnInit(): void {
+    if( localStorage.getItem("token") == null || localStorage.getItem("token") == "null" ){
+      this.router.navigate(['/login']);
+    }
+    this.u = this.api.getUsuario(+localStorage.getItem("userID"));
   }
 
 }
