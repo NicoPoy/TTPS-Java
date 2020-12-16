8ffc8164d90aaa3064c032867733d126f86f6c7f
Nicolás Poy <45112236+NicoPoy@users.noreply.github.com>
2020-12-16 20:31:20 -0300

    vista editar

--- FoodTrucks/src/app/editar-perfil/editar-perfil.component.ts
+++ FoodTrucks/src/app/editar-perfil/editar-perfil.component.ts
@@ -13,29 +13,38 @@ export class EditarPerfilComponent implements OnInit {
 
   u: Usuario;
 
-  actUsuarioForm = new FormGroup({});
+  actUsuarioForm = new FormGroup({
+        nombre: new FormControl(),
+        apellido: new FormControl(),
+        username: new FormControl(),
+        password: new FormControl(),
+  });
 
   constructor(private api:ApiService, private router:Router) { }
 
   ngOnInit(): void {
+    
     if( localStorage.getItem("token") == null || localStorage.getItem("token") == "null" ){
       this.router.navigate(['/login']);
     }
+
     this.api.getUsuario(localStorage.getItem("userID")).subscribe(data => {
+      
       this.u = data;
-      this.actUsuarioForm.addControl('nombre', new FormControl(this.u.nombre,[Validators.required, Validators.minLength(3)] ));
-      this.actUsuarioForm.addControl('apellido', new FormControl(this.u.apellido,[Validators.required, Validators.minLength(3)] ));
-      this.actUsuarioForm.addControl('username', new FormControl(this.u.username,[Validators.required, Validators.minLength(3)] ));
-      this.actUsuarioForm.addControl('password', new FormControl(this.u.password,[Validators.required, Validators.minLength(3)] ));
+
+      this.actUsuarioForm.setControl('nombre', new FormControl(this.u.nombre,[Validators.required, Validators.minLength(3)] ));
+      this.actUsuarioForm.setControl('apellido', new FormControl(this.u.apellido,[Validators.required, Validators.minLength(3)] ));
+      this.actUsuarioForm.setControl('username', new FormControl(this.u.username,[Validators.required, Validators.minLength(3)] ));
+      this.actUsuarioForm.setControl('password', new FormControl(this.u.password,[Validators.required, Validators.minLength(3)] ));
+
       /* this.actUsuarioForm = new FormGroup({
         nombre: new FormControl(this.u.nombre, [Validators.required, Validators.minLength(3)] ),
         apellido: new FormControl(this.u.apellido, [Validators.required, Validators.minLength(3)] ),
         username: new FormControl(this.u.username, [Validators.required, Validators.minLength(3)] ),
         password: new FormControl(this.u.password, [Validators.required, Validators.minLength(3)] ),
       }) */
-    });
-
-   
+    
+    });   
 
   }
 
